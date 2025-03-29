bdd73cc4023007e1827a777bacd26d6989edc2ba
Manya Vikram <144259892+ManyaVikram@users.noreply.github.com>
2025-03-29 16:34:48 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,18 @@
+"use strict"; //use all JS code as newer version
+
+// alert( 3 + 3 ) //we are using nodejs, not browser
+
+let name = "Manya"
+let age = 18
+let isLoggedIn = false
+
+// number => 2 to power of 53 //range in memory address
+// bigint //in trading, stock market, facebook
+// string //always used in ""
+// boolean for true or false 
+// null //it is a datatype and standalone value//represntation of empty value//returns object
+// undefined //not assigned any value // returns undefined
+// symbol //used in figma to specify each component
+
+//object
+console.log(typeof undefined);
