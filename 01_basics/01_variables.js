eac7a54d734ed0ef3fa84e53cc24b74426b6ce15
Manya Vikram <144259892+ManyaVikram@users.noreply.github.com>
2025-03-29 11:58:57 +0000

    done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 144553
+let accountEmail = "manya@google.com"
+var accountPassword = "12345"
+accountCity = "Patna"
+let accountState;
+
+// accountId = 2 // not allowed
+
+accountEmail = "mm@mm.com"
+accountPassword = "12231223"
+accountCity = "Lucknow"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
